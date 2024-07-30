b2d08566578e70cea5134e01b02412f959310e77
shiotoli <shiotoli@gmail.com>
2024-07-30 11:56:21 +0800

    example

--- offchain/nucleus-cage/src/vm.rs
+++ offchain/nucleus-cage/src/vm.rs
@@ -43,19 +43,21 @@ impl Vm {
             .instance
             .get_func(&mut self.space, func)
             .ok_or(anyhow::anyhow!("endpoint not found"))?;
-        let mm = self
-            .instance
-            .get_export(&mut self.space, "memory")
-            .ok_or(anyhow::anyhow!("no memory exported"))?
-            .into_memory()
-            .ok_or(anyhow::anyhow!("no memory exported"))?;
-        mm.write(&mut self.space, 1, &args)?;
-        let mut results = vec![Val::I32(0), Val::I32(0)];
+        // let mm = self
+        //     .instance
+        //     .get_export(&mut self.space, "memory")
+        //     .ok_or(anyhow::anyhow!("no memory exported"))?
+        //     .into_memory()
+        //     .ok_or(anyhow::anyhow!("no memory exported"))?;
+        // mm.write(&mut self.space, 1, &args)?;
+        // let mut results = vec![Val::I32(0), Val::I32(0)];
+        let mut results = vec![];
         post_fn.call(
             &mut self.space,
-            &[Val::I32(1), Val::I32(args.len() as i32)],
+            &[Val::I32(2), Val::I32(1), Val::I32(args.len() as i32)],
             &mut results,
         )?;
+        println!("results: {:?}", results);
         log::info!("results: {:?}", results);
         Ok(vec![0u8])
     }
@@ -97,14 +99,62 @@ mod tests {
             pub c: u32,
         }
         let e = E {
-            a: vec![],
+            a: vec![0],
             b: 0,
             c: 1,
         };
         let encoded_args = e.encode();
-        assert_eq!(
-            vec![0u8],
-            vm.call_post("__nucleus_post_post", encoded_args).unwrap()
+        println!(
+            "encoded_args: {:?}",
+            vm.call_post("__nucleus_post_post", encoded_args)
         );
+        // assert_eq!(
+        //     vec![0u8],
+        //     vm.call_post("__nucleus_post_post", encoded_args).unwrap()
+        // );
+    }
+    #[test]
+    pub fn call_post_should_work() {
+        env_logger::init();
+        let wasm_path = "../../target/wasm32-unknown-unknown/debug/vrs_nucleus_examples.wasm";
+
+        let engine = Engine::default();
+        let module = Module::from_file(&engine, wasm_path).unwrap();
+        module.exports().for_each(|ty| match ty.ty() {
+            ExternType::Func(func) => {
+                println!("export: {} {}", func.to_string(), ty.name());
+            }
+            _ => {}
+        });
+        let mut store = Store::new(&engine, Context::init().unwrap());
+        let injects = Context::inject_host_funcs(&mut store);
+        let instance = Instance::new(&mut store, &module, &injects).unwrap();
+        let memory = instance
+            .get_memory(&mut store, "memory")
+            .expect("Failed to get memory");
+        let input = b"Hello, a world with a lot of a characters!";
+        let input_len = input.len().min(255);
+
+        let ptr = memory.data_size(&store) as i32;
+        memory.grow(&mut store, 1).unwrap();
+        memory
+            .write(&mut store, ptr as usize, &input[..input_len])
+            .unwrap();
+
+        let add = instance
+            .get_func(&mut store, "replace_a_with_b")
+            .expect("function not found");
+        let mut result = vec![Val::I32(0)];
+        let results = add
+            .call(
+                &mut store,
+                &[Val::I32(ptr), Val::I32(input_len as i32)],
+                &mut result,
+            )
+            .unwrap();
+        let result_ptr = result[0].unwrap_i32() as usize;
+        let mut result = vec![0u8; input_len];
+        memory.read(&store, result_ptr, &mut result).unwrap();
+        println!("results: {:?}", String::from_utf8(result).unwrap());
     }
 }
--- offchain/nucleus-examples/src/lib.rs
+++ offchain/nucleus-examples/src/lib.rs
@@ -25,9 +25,19 @@ pub fn init(e: t::E, u: u32) {
 #[post]
 pub fn post(e: E) -> Result<D, ()> {
     storage::put(b"hello", b"world").map_err(|_| ())?;
-    Ok(D { b: 1 })
+    Ok(D { b: 1582 })
 }
 
+#[no_mangle]
+pub fn replace_a_with_b(ptr: *mut u8, len: usize) -> *mut u8 {
+    let slice = unsafe { std::slice::from_raw_parts_mut(ptr, len) };
+    for byte in slice.iter_mut() {
+        if *byte == b'a' {
+            *byte = b'b';
+        }
+    }
+    ptr
+}
 #[post("postOne")]
 pub fn post1(e: E) {
     storage::put(b"hello", b"world").unwrap();
