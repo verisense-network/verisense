ee10ada6a5cc789228199c31342c79829bc88e1a
yukihonda0x1 <185462284+yukihonda0x1@users.noreply.github.com>
2024-10-22 16:48:31 +0800

    update

---
+++ support/src/lib.rs
@@ -0,0 +1,16 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use sp_std::vec::Vec;
+#[macro_export]
+macro_rules! log {
+	($level:tt, $patter:expr $(, $values:expr)* $(,)?) => {
+		log::$level!(
+			target: crate::LOG_TARGET,
+			concat!("[{:?}] ", $patter), <frame_system::Pallet<T>>::block_number() $(, $values)*
+		)
+	};
+}
+
+pub trait ElValidatorProvider<AccountId> {
+    fn provide() -> Vec<(AccountId, u128)>;
+}
