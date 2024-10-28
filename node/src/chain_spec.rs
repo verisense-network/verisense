76aa6b657f909f450f18826ea8a1d11a2e9939ed
yukihonda0x1 <185462284+yukihonda0x1@users.noreply.github.com>
2024-10-28 16:35:17 +0800

    impl validators (#45)
    * impl validators

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -4,6 +4,7 @@ use sp_consensus_grandpa::AuthorityId as GrandpaId;
 use sp_core::{sr25519, Pair, Public};
 use sp_runtime::traits::{IdentifyAccount, Verify};
 use vrs_runtime::{AccountId, Signature, WASM_BINARY};
+use vrs_runtime::opaque::SessionKeys;
 
 // The URL for the telemetry server.
 // const STAGING_TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
@@ -29,8 +30,8 @@ where
 }
 
 /// Generate an Aura authority key.
-pub fn authority_keys_from_seed(s: &str) -> (AuraId, GrandpaId) {
-    (get_from_seed::<AuraId>(s), get_from_seed::<GrandpaId>(s))
+pub fn authority_keys_from_seed(s: &str) -> (AccountId, AuraId, GrandpaId) {
+    (get_account_id_from_seed::<sr25519::Public>(&s), get_from_seed::<AuraId>(s), get_from_seed::<GrandpaId>(s))
 }
 
 pub fn development_config() -> Result<ChainSpec, String> {
@@ -97,7 +98,7 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 
 /// Configure initial storage state for FRAME modules.
 fn testnet_genesis(
-    initial_authorities: Vec<(AuraId, GrandpaId)>,
+    initial_authorities: Vec<(AccountId, AuraId, GrandpaId)>,
     root_key: AccountId,
     endowed_accounts: Vec<AccountId>,
     _enable_println: bool,
@@ -107,15 +108,36 @@ fn testnet_genesis(
             // Configure endowed accounts with initial balance of 1 << 60.
             "balances": endowed_accounts.iter().cloned().map(|k| (k, 1u64 << 60)).collect::<Vec<_>>(),
         },
-        "aura": {
-            "authorities": initial_authorities.iter().map(|x| (x.0.clone())).collect::<Vec<_>>(),
-        },
-        "grandpa": {
-            "authorities": initial_authorities.iter().map(|x| (x.1.clone(), 1)).collect::<Vec<_>>(),
-        },
         "sudo": {
             // Assign network admin rights.
             "key": Some(root_key),
         },
+         "session":  {
+            "keys": initial_authorities
+                .iter()
+                .map(|x| {
+                    (
+                        x.0.clone(),
+                        x.0.clone(),
+                        session_keys(
+                            x.1.clone(),
+                            x.2.clone(),
+                        ),
+                    )
+                })
+                .collect::<Vec<_>>(),
+        },
     })
 }
+
+
+fn session_keys(
+    aura: AuraId,
+    grandpa: GrandpaId,
+) -> SessionKeys {
+    SessionKeys {
+        aura,
+        grandpa,
+    }
+}
+
---
+++ support/src/lib.rs
@@ -0,0 +1,17 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use sp_std::vec::Vec;
+#[macro_export]
+macro_rules! log {
+	($level:tt, $patter:expr $(, $values:expr)* $(,)?) => {
+		log::$level!(
+			target: crate::LOG_TARGET,
+			concat!("[{:?}] ", $patter), <frame_system::Pallet<T>>::block_number() $(, $values)*
+		)
+	};
+}
+
+pub trait ValidatorsProvider<AccountId> {
+	fn provide() -> Vec<(AccountId, u128)>;
+	fn next_validators_set_id() -> u32;
+}
