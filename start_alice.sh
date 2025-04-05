d08ba1de2842938156aaca051838d6f397cded2e
shiotoli <shiotoli@gmail.com>
2025-04-05 15:20:23 +0800

    merge

--- start_alice.sh
+++ start_alice.sh
@@ -1,8 +1,13 @@
-/data/verisense \
---base-path /data/config \
---chain /data/betanet.json \
---port 30333 \
---rpc-port 9944 \
---validator \
---rpc-cors=all \
---tss-coordinator 12944
+#!/usr/bin/env sh
+
+(
+sudo /data/verisense \
+  --base-path /data/config \
+  --chain /data/betanet.json \
+  --port 30333 \
+  --rpc-port 9944 \
+  --rpc-cors=all \
+  --validator \
+  --tss-coordinator 12944 \
+  2>&1 | tee -a /data/verisense.out
+) &
--- start_dave.sh
+++ start_dave.sh
@@ -1,11 +1,15 @@
-/data/verisense \
---base-path /data/config \
---chain /data/betanet.json \
---port 30333 \
---rpc-port 9944 \
---validator \
---tss-signer /ip4/10.128.0.2/tcp/12944/p2p/12D3KooWFcGs16mdf3HuNd2KMx5WYNsDyyDVz9h6Udg6WWg3CCxh
+#!/usr/bin/env sh
 
+(
+sudo /data/verisense \
+  --base-path /data/config \
+  --chain /data/betanet.json \
+  --port 30333 \
+  --rpc-port 9944 \
+  --validator \
+  --tss-signer /ip4/10.128.0.2/tcp/12944/p2p/12D3KooWFcGs16mdf3HuNd2KMx5WYNsDyyDVz9h6Udg6WWg3CCxh \
+  2>&1 | tee /data/verisense.out
+)&
 
 # ./target/release/verisense key generate-node-key --file /tmp/dave/node-key
 # ./target/release/verisense \
